aed2662ac25d89e51f5cafe16d42e5e0f7de6132
Michael Kulagin <mvkulagin@bk.ru>
2023-03-27 11:41:57 +0400

    save in LocalStorage

--- src/app/store.js
+++ src/app/store.js
@@ -1,6 +1,34 @@
 import { configureStore } from '@reduxjs/toolkit'
 import reducer from '../reducers/reducers'
 
+const loadState = () => {
+let preloadedState = localStorage.getItem('reduxState');
+   if(preloadedState === null){
+    return preloadedState = 
+    {
+
+        visibleInputs: false,
+        visibleAddUsefulManButton: true,
+        visibleCategoryInput: false,
+        visibleCategoryPanel: true,
+        manIdEditMode: undefined,
+        usefulPeople:{}
+    }
+ }else {
+        return JSON.parse(preloadedState)
+    }
+}
+
 export const store = configureStore({
-    reducer: reducer
+    reducer: reducer,
+    preloadedState: loadState()
+    
 })
+
+console.log(store);
+console.log(loadState())
+
+store.subscribe(()=>{
+    localStorage.setItem('reduxState', JSON.stringify(store.getState()))
+})
+
