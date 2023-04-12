4894a0ff4d7c87d018928444c3e24596a09c16c8
Michael Kulagin <mvkulagin@bk.ru>
2023-04-12 13:07:21 +0400

    12/04/23

--- src/components/inputNewManComponents/InputData.js
+++ src/components/inputNewManComponents/InputData.js
@@ -2,7 +2,7 @@ import React from "react";
 
 
 function InputData({value,changeHandler, objKey, placeholder}){
-    return <input className="inpiut-data-man"
+    return <input className="input-data-man"
      value={value}
      placeholder={placeholder}
      onChange={event=>changeHandler(objKey,event)}/>
